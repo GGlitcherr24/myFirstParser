6d4a1cf421e333ff545a1a6067d2dc273cf6cba6
Антон Нестеров <naa@office.hlcompany.ru>
2024-02-28 10:42:42 +0400

    Полностью товары

--- main.py
+++ main.py
@@ -41,13 +41,13 @@ def AppendCat(resultCat, category):
                           'NAME': category['items'][i]['name'],
                           'MEASURE_UNIT': '', 'UNIT_WEIGHT': '', 'ENERGY': '', 'FIBER': '', 'FAT': '',
                           'CARBOHYDRATE': '', 'PRICE': ''})
+        print(i)
     return resultCat
 
 
-linkNom = "https://new.moykassir.ru/api/nomenclature?page=1&limit=50&include_nested_categories=true"
 linkCat = "https://new.moykassir.ru/api/categories?need_hierarchy=true"
 headersNom = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbmV3Lm1veWthc3Npci5ydVwvYXBpXC9hdXRoXC9yZWZyZXNoIiwiaWF0IjoxNzA5MDk4NDY4LCJleHAiOjE3MDkxMDIwODEsIm5iZiI6MTcwOTA5ODQ4MSwianRpIjoiSE0xVTJkM0poa0ROODJ2SSIsInN1YiI6MzQ2MTUsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJlbWFpbCI6Im1tdkBobGNvbXBhbnkucnUifQ.VCA3rzNru-dS8_7qzyox7PZWrW8rgAmkxY9pqgyEwno'
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbmV3Lm1veWthc3Npci5ydVwvYXBpXC9hdXRoXC9yZWZyZXNoIiwiaWF0IjoxNzA5MTAwMjk1LCJleHAiOjE3MDkxMDU5MjEsIm5iZiI6MTcwOTEwMjMyMSwianRpIjoienRpbEszaVdROUVlN2ZiYiIsInN1YiI6MzQ2MTUsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJlbWFpbCI6Im1tdkBobGNvbXBhbnkucnUifQ.DdMPKiq7waeMSkVH_-ZT_yuy0TG7LjxqUnj5xqux3qc'
 }
 headersCat = {
     'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbmV3Lm1veWthc3Npci5ydVwvYXBpXC9hdXRoXC9yZWZyZXNoIiwiaWF0IjoxNzA5MDk4NDY4LCJleHAiOjE3MDkxMDIyNjAsIm5iZiI6MTcwOTA5ODY2MCwianRpIjoid0h6NDZyQzZhbjRiQzNvVSIsInN1YiI6MzQ2MTUsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJlbWFpbCI6Im1tdkBobGNvbXBhbnkucnUifQ.1POUDSy4zu33QQtBPxsv7WVoyWGEHkA5cIRawwspOao'
@@ -56,12 +56,16 @@ headersCat = {
 
 headlines = ['TYPE', 'NUM', 'PARENT_CODE', 'NAME', 'MEASURE_UNIT', 'UNIT_WEIGHT', 'ENERGY', 'FIBER', 'FAT', 'CARBOHYDRATE', 'PRICE']
 
-nomenclature = GetRequest(linkNom, headersNom).json()
-category = GetRequest(linkCat, headersCat).json()
-print(category['items'][0])
 resultNom, resultCat = [], []
-#resultNom = AppendGoods(resultNom, nomenclature)
-resultCat = AppendCat(resultCat, category)
+
+for i in range(1,9):
+    linkNom = "https://new.moykassir.ru/api/nomenclature?page=" + str(i) + "&limit=50&include_nested_categories=true"
+    print(linkNom)
+    nomenclature = GetRequest(linkNom, headersNom).json()
+    resultNom = AppendGoods(resultNom, nomenclature)
+#category = GetRequest(linkCat, headersCat).json()
+#print(category['items'][0])
+#resultCat = AppendCat(resultCat, category)
 
 
 # for i in resultNom:
@@ -73,10 +77,10 @@ resultCat = AppendCat(resultCat, category)
 
 with open("nomenclature.csv", 'a', encoding='utf-8') as file:
     file_writer = csv.writer(file, delimiter=";", lineterminator="\r")
-    # file_writer.writerow(headlines)
-    # for i in resultNom:
-    #     file_writer.writerow(i.values())
-    for i in resultCat:
+    file_writer.writerow(headlines)
+    for i in resultNom:
         file_writer.writerow(i.values())
+    # for i in resultCat:
+    #     file_writer.writerow(i.values())
 
 
