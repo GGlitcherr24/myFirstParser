dfa99e1ae818b1c9b1274f391cc601a5289a183e
Антон Нестеров <naa@office.hlcompany.ru>
2024-02-29 11:15:56 +0400

    С прошлым кодом

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import requests
 import pprint as pp
 import csv
+import sys
 
-def Token():
-    url = ''
-    json = {"Content-type": 'application/json',
-            "login": "mmv@hlcompany.ru",
-            "password": "6ZtqeJ"}
-    response = requests.post(url=url, json=json)
-    print(response)
-    return response.json()
-
+headers = {
+    'Authorization': ''
+}
+headlines = ['TYPE', 'NUM', 'PARENT_CODE', 'NAME', 'MEASURE_UNIT', 'UNIT_WEIGHT', 'ENERGY', 'FIBER', 'FAT', 'CARBOHYDRATE', 'PRICE', 'CATEGORY']
 
 def GetRequest(link, headers):
     response = requests.get(link, headers=headers)
     print(response)
-    return response
+    return response.json()
+
+def main():
+    resultNom, resultCat = [], []
+    i = 1
+    while True:
+        link = "https://new.moykassir.ru/api/nomenclature?page=" + str(i) + "&limit=50&include_nested_categories=true"
+        nomenclatureGoods = GetRequest(link, headers=headers)
+        if nomenclatureGoods['pager']['hasNext'] == True:
+            resultNom = AppendGoods(resultNom, nomenclatureGoods)
+        else:
+            resultNom = AppendGoods(resultNom, nomenclatureGoods)
+            break
+    nomenclatureCategory = GetRequest("https://new.moykassir.ru/api/categories?need_hierarchy=true", headers=headers)
+    resultCat = AppendCat(resultCat, nomenclatureCategory)
+    WriterFile(resultNom, resultCat)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        headers['Authorization'] = 'Bearer ' + sys.argv[1]
+    main()
 
 def AppendGoods(resultNum, numenlature):
     for i in range(0, len(numenlature['items'])):
@@ -45,30 +62,14 @@ def AppendCat(resultCat, category):
 
     return resultCat
 
-
-linkCat = "https://new.moykassir.ru/api/categories?need_hierarchy=true"
-headersNom = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbmV3Lm1veWthc3Npci5ydVwvYXBpXC9hdXRoXC9yZWZyZXNoIiwiaWF0IjoxNzA5MTAwMjk1LCJleHAiOjE3MDkxMjEzMzIsIm5iZiI6MTcwOTExNzczMiwianRpIjoiMmtjVFhreFdGOXNGYzFzaiIsInN1YiI6MzQ2MTUsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJlbWFpbCI6Im1tdkBobGNvbXBhbnkucnUifQ.WgBppLpx3nA1OaKdW18D36PQ0Vjrk6MnUusekf0n1EI'
-}
-headersCat = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbmV3Lm1veWthc3Npci5ydVwvYXBpXC9hdXRoXC9yZWZyZXNoIiwiaWF0IjoxNzA5MTAwMjk1LCJleHAiOjE3MDkxMjEzMzIsIm5iZiI6MTcwOTExNzczMiwianRpIjoiMmtjVFhreFdGOXNGYzFzaiIsInN1YiI6MzQ2MTUsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJlbWFpbCI6Im1tdkBobGNvbXBhbnkucnUifQ.WgBppLpx3nA1OaKdW18D36PQ0Vjrk6MnUusekf0n1EI'
-}
-
-
-headlines = ['TYPE', 'NUM', 'PARENT_CODE', 'NAME', 'MEASURE_UNIT', 'UNIT_WEIGHT', 'ENERGY', 'FIBER', 'FAT', 'CARBOHYDRATE', 'PRICE', 'CATEGORY']
-
-resultNom, resultCat = [], []
-
 # for i in range(1,9):
-#     linkNom = "https://new.moykassir.ru/api/nomenclature?page=" + str(i) + "&limit=50&include_nested_categories=true"
+      #linkNom = "https://new.moykassir.ru/api/nomenclature?page=" + str(i) + "&limit=50&include_nested_categories=true"
 #     print(linkNom)
 #     nomenclature = GetRequest(linkNom, headersNom).json()
 #     resultNom = AppendGoods(resultNom, nomenclature)
-category = GetRequest(linkCat, headersCat).json()
-print(category['items'][0])
-resultCat = AppendCat(resultCat, category)
-
-
+# category = GetRequest(linkCat, headersCat).json()
+# print(category['items'][0])
+# resultCat = AppendCat(resultCat, category)
 # for i in resultNom:
 #     print(i)
 # print(len(resultNom))
@@ -76,11 +77,12 @@ resultCat = AppendCat(resultCat, category)
 # print("TEST ______________________")
 # print(nomenclature['items'][1]['name'])
 
-with open("nomenclature.csv", 'a', encoding='utf-8') as file:
-    file_writer = csv.writer(file, delimiter=";", lineterminator="\r")
-    # file_writer.writerow(headlines)
-    # for i in resultNom:
-    #     file_writer.writerow(i.values())
-    for i in resultCat:
-        file_writer.writerow(i.values())
+def WriterFile(resultNom, resultCat):
+    with open("nomenclature.csv", 'a', encoding='utf-8') as file:
+        file_writer = csv.writer(file, delimiter=";", lineterminator="\r")
+        file_writer.writerow(headlines)
+        for i in resultNom:
+            file_writer.writerow(i.values())
+        for i in resultCat:
+            file_writer.writerow(i.values())
 
